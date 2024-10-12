9bbe394bdd117c26e85ff6516a65ea4e60b3efed
Rahul Gaur <rgaur1620@gmail.com>
2024-10-12 22:49:59 +0530

    added Jacoco library for code coverage

--- kafka-producer/src/test/java/com/rahulsgf/kafka_producer/controller/HomeControllerTest.java
+++ kafka-producer/src/test/java/com/rahulsgf/kafka_producer/controller/HomeControllerTest.java
@@ -3,12 +3,14 @@ package com.rahulsgf.kafka_producer.controller;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 @Slf4j
+@WebMvcTest(HomeController.class)
 class HomeControllerTest {
 
     @Autowired

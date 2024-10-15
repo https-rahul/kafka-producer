a456a949d6f2cb0f1d9820293507615d4e152099
Rahul Gaur <rgaur1620@gmail.com>
2024-10-15 17:09:48 +0530

    added CSRF based token
    work still in progress

--- kafka-producer/src/main/java/com/rahulsgf/kafka_producer/controller/CsrfController.java
+++ kafka-producer/src/main/java/com/rahulsgf/kafka_producer/controller/CsrfController.java
@@ -1,16 +1,15 @@
 package com.rahulsgf.kafka_producer.controller;
 
 import jakarta.servlet.http.HttpServletRequest;
-import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.web.csrf.CsrfToken;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@Slf4j
-public class HomeController {
+public class CsrfController {
 
-    @GetMapping("/")
-    public String home(HttpServletRequest request) {
-        return "Application started: Welcome user! \nSessionID: " +request.getSession().getId();
+    @GetMapping("/csrf")
+    public CsrfToken getCsrfToken(HttpServletRequest request) {
+        return (CsrfToken) request.getAttribute("_csrf");
     }
 }

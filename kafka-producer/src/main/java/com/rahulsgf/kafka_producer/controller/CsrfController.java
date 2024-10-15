6beb2f35df9dab0e6a3f54a222d1a0595da3eea6
Rahul Gaur <rgaur1620@gmail.com>
2024-10-15 20:08:31 +0530

    implemented SessionID and CSRF based token authentication to accept default user's request.

--- kafka-producer/src/main/java/com/rahulsgf/kafka_producer/controller/CsrfController.java
+++ kafka-producer/src/main/java/com/rahulsgf/kafka_producer/controller/CsrfController.java
@@ -1,15 +1,29 @@
 package com.rahulsgf.kafka_producer.controller;
 
 import jakarta.servlet.http.HttpServletRequest;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.web.csrf.CsrfToken;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 public class CsrfController {
 
-    @GetMapping("/csrf")
+    //Cross Site Request Forgery
+    @GetMapping("/auth")
     public CsrfToken getCsrfToken(HttpServletRequest request) {
-        return (CsrfToken) request.getAttribute("_csrf");
+
+        CsrfToken csrfToken = (CsrfToken) request.getAttribute("_csrf");
+
+        if (csrfToken != null){
+            log.info("CSRF TOKEN GENERATED: param name={}, value={}, header={}", csrfToken.getParameterName(), csrfToken.getToken(), csrfToken.getHeaderName());
+            return (CsrfToken)request.getAttribute("_csrf");
+
+        } else {
+            log.warn("CSRF TOKEN NOT PRESENT IN THE REQUEST");
+        }
+
+        return null;
     }
 }
